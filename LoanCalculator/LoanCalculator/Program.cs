99e89a61cb730b7668bbb36bbe45d0b112c6cc74
GKavvadias <80004643+GKavvadias@users.noreply.github.com>
2021-05-23 12:37:04 +0300

    Add files via upload

---
+++ LoanCalculator/LoanCalculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please input your capital: ");
+            double capital = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.Write("Please input the number of installments: ");
+            double installments = Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            Console.Write("Please input the interest rate: ");
+            double interest_rate =Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+
+            interest_rate = interest_rate / 100;
+
+            double installment = capital * interest_rate / (1 - Math.Pow((1 + interest_rate), (-1 * installments)));
+            Math.Round(installment, 2);
+
+            Console.WriteLine("1.Capital is: " + capital);
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("2.Number of installments is: " + installments);
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("3.Interest rate is: " + interest_rate);
+            Console.WriteLine("===================================");
+            Console.WriteLine("4.Your installment is: " + Math.Round(installment, 2) + " euros");
+
+            Console.ReadKey();
+        }
+    }
+}
